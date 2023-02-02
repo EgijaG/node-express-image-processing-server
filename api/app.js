a157d114d29fd06899517b26b88b2a9ca741012a
EgijaG <57557140+EgijaG@users.noreply.github.com>
2023-02-02 17:58:02 +0200

    Module 3 done

--- api/app.js
+++ api/app.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+const router = require('./scr/router');
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
 });
-
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 module.exports = app;
