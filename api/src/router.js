81115775e58ec3bbe0f45c22bf482627a6ba7486
EgijaG <57557140+EgijaG@users.noreply.github.com>
2023-02-20 18:01:25 +0200

    Module 4

--- api/src/router.js
+++ api/src/router.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
-
+const imageProcessor = require('./imageProcessor');
 // eslint-disable-next-line new-cap
 const router = Router();
 
@@ -22,8 +22,11 @@ const fileFilter = (request, file, callback) => {
 
 const upload = multer({fileFilter, storage});
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
+    try {
+      await imageProcessor(request.file.filename);
+    } catch (error) {}
     return response.status(400).json({error: request.fileValidationError});
   } else {
     return response.status(201).json({success: true});
